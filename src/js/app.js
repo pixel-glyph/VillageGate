d12894bcac6eb9700a8443fd31dc91d0504acd9a
Andrew <arjbattaglia@gmail.com>
2016-02-10 14:09:39 -0500

    fixed header bugs

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,5 @@
 
 const $ = require('jquery');
-
 let st,
     didScroll,
     lastScrollTop = 0,
@@ -8,48 +7,38 @@ let st,
     navbarHeight = $('.home-header').outerHeight();
 
 $(window).scroll(function(){
-  didScroll = true;
+  //didScroll = true;
   st = $(this).scrollTop();
 
-  $('.top-img').css({
-    'transform': 'traslate(0, 20%)'
-  });
+  if (st < 800) {
+    $('.top-img').css({
+      'transform': 'traslate(0, 20%)'
+    });
 
-  $('.logo').css({
-    'transform': 'translate(0,' + st /6.5 + '%)',
-    'opacity': 1 - (st / 750)
-  });
+    $('.logo').css({
+      'transform': 'translate(0,' + st /6.5 + '%)',
+      'opacity': 1 - (st / 750)
+    });
+  }
 
-});
-
-setInterval(function() {
-	if (didScroll) {
-		hasScrolled();
-		didScroll = false;
-	}
-}, 100);
-
-
-function hasScrolled() {
-
-  // Make sure they scroll more than delta
+  // scroll more than delta
 	if (Math.abs(lastScrollTop - st) <= delta)
 		return;
 
-	// If they scrolled down and are past the navbar, add class .nav-up.
-	// This is necessary so you never see what is "behind" the navbar.
-	if (st > lastScrollTop && st > navbarHeight){
-		// Scroll Down
+	// If scrolling down and past navbar
+	if (st > lastScrollTop && st > navbarHeight) {
 		$('.home-header').addClass('nav-up');
 	} else {
-		// Scroll Up
+    // Top of page
     if (st === 0) {
       $('.home-header').removeClass('on-screen');
-    } else if(st + $(window).height() < $(document).height()) {
+      $('.home-header').removeClass('nav-up');
+      // Scroll Up
+    } else if (st + $(window).height() < $(document).height() && st > navbarHeight) {
       $('.home-header').addClass('on-screen');
       $('.home-header').removeClass('nav-up');
     }
 	}
 
 	lastScrollTop = st;
-}
+});
